639ce5934e24869128a4af3f25f9e51feb7a7c77
RubiEmelly <emellyandrade.rubiyasmin@gmail.com>
2024-02-22 13:41:41 -0300

    Feat: First commit

---
+++ api/Data/SigedespDBContex.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sigedesp.Models;
+
+namespace Sigedesp.Data
+{
+    public class SigedespDBContex : DbContext
+    {
+        public SigedespDBContex(DbContextOptions<SigedespDBContex> options) : base(options) 
+        {
+        }
+
+        public DbSet<TipoDespesaModel> TipoDespesa { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ api/Repositorios/Interfaces/ITipoDespesaRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using Sigedesp.Models;
+
+namespace Sigedesp.Repositorios.Interfaces
+{
+    public interface ITipoDespesaRepositorio
+    {
+        Task<List<TipoDespesaModel>> BuscarTipoDespesa();
+        Task<TipoDespesaModel> BuscarPorId(int id);
+        Task<TipoDespesaModel> Adicionar(TipoDespesaModel tipoDespesa);
+        Task<TipoDespesaModel> Atualizar(TipoDespesaModel tipoDespesa, int id);
+        Task<bool> Apagar(int id);
+    }
+}
